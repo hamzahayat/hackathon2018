a1d316374bea81a9a6ea6e15dad8ab3b3b7c83e5
Kuntesh Patel <khpatel@apigarage.com>
2018-11-17 20:50:05 -0500

    init design

--- protectx/src/App.js
+++ protectx/src/App.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./images/ProtectX.png";
 let web3;
 
 class App extends Component {
-
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
     window.onload = () => {
-      web3 = window.aionweb3
-    }
+      web3 = window.aionweb3;
+    };
   }
 
   handleSignTransaction = async () => {
     const tx = {
-      to: '0xa020f60b3f4aba97b0027ca81c5d20c9898d7c43a50359d209596b86e8ce3ca2',
+      to: "0xa020f60b3f4aba97b0027ca81c5d20c9898d7c43a50359d209596b86e8ce3ca2",
       value: 0,
       data: "",
       from: web3.eth.accounts[0]
     };
 
-    const signedTx = await web3.eth.signTransaction(tx)
+    const signedTx = await web3.eth.signTransaction(tx);
     console.log(signedTx);
-  }
+  };
 
   handleCancelChecks = async () => {
     const tx = {
-      to: '0xa020f60b3f4aba97b0027ca81c5d20c9898d7c43a50359d209596b86e8ce3ca2',
+      to: "0xa020f60b3f4aba97b0027ca81c5d20c9898d7c43a50359d209596b86e8ce3ca2",
       value: 0,
       data: "",
       from: web3.eth.accounts[0]
@@ -37,24 +36,44 @@ class App extends Component {
 
     const txHash = await web3.eth.sendTransaction(tx);
     console.log(txHash);
-  }
-
+  };
 
   render() {
     return (
-      <div>
-        <button
-          className="button is-primary"
-          onClick={this.handleSignTransaction}
-        >
-          Sign transactions
-            </button>
-        <button
-          className="button is-primary"
-          onClick={this.handleCancelChecks}
-        >
-          cancel checks
-</button>
+      <div className="home">
+        <div className="">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="labelTo">
+          <label>To(Address)</label>
+        </div>
+        <div className="txtTo">
+          <input type="text" />
+        </div>
+        <div className="labelApi">
+          <label>Api</label>
+        </div>
+        <div className="txtApi">
+          <input type="text" />
+        </div>
+    
+        <div className="sign">
+          <button
+            className="button is-primary"
+            onClick={this.handleSignTransaction}
+          >
+            {" "}
+            Sign transactions
+          </button>
+        </div>
+        <div className="cancel">
+          <button
+            className="button is-primary"
+            onClick={this.handleCancelChecks}
+          >
+            cancel checks
+          </button>
+        </div>
       </div>
     );
   }
